059e8624c381439e5810638333afbe8f6edc37d3
Thomas Stringer <tr.stringer@outlook.com>
2016-08-16 13:54:06 -0400

    test continuous deployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function (context, myQueueItem) {
-    context.log('Node.js queue trigger function processed work item', myQueueItem);
+    context.log('Deployment1 Node.js queue trigger function processed work item', myQueueItem);
     context.done();
 };
