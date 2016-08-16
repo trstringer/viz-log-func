a657567bd79d9cacda9cf668a0ed0c37dc009524
Thomas Stringer <tr.stringer@outlook.com>
2016-08-16 16:21:48 -0400

    add ddb create functionality

--- log-handler/index.js
+++ log-handler/index.js
@@ -1,4 +1,21 @@
+const DocumentClient = require('documentdb').DocumentClient;
+
 module.exports = function (context, myQueueItem) {
-    context.log('Deployment1 Node.js queue trigger function processed work item', myQueueItem);
-    context.done();
+    // context.log('Deployment1 Node.js queue trigger function processed work item', myQueueItem);
+
+    const client = new DocumentClient(process.env.DDB_HOST, {masterKey: process.env.DDB_KEY});
+
+    const databaseUri = 'dbs/viz-access';
+    const collectionUri = `${databaseUri}/colls/facial-recog`;
+
+    client.createDocument(collectionUri, myQueueItem, (err, resource, response) => {
+        if (err) {
+            context.log(err.message);
+        }
+        else {
+            context.log('successfully inserted document in ddb');
+        }
+
+        context.done();
+    });
 };
